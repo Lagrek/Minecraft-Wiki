5d186f2dec8e3c8277dfb2ee992abb15cde72371
Lagrek <rain.demorser@gmail.com>
2018-09-28 16:54:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var c=4;
+    sum1=1;
+switch(1) 
+{
+          case 1:{
+             if(sum1<c);
+       console.log("Yes");
+         else
+       console.log("No");
+         break; 
+             }
+       case 2:{
+             console.log ("Kornelii");
+         break; 
+             }
+       case 3:{
+             console.log ("Vodomir");
+         break; 
+             }
+        default:{
+             console.log ("I don't know");
+         break; 
+             }
+}
