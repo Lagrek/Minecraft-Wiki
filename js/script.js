9da5f56ca2a76143a5e1f97eea806ec89a622249
Lagrek <rain.demorser@gmail.com>
2018-10-02 16:41:36 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,72 @@
-var c=4;
-    sum1=1;
-switch(1) 
-{
-          case 1:{
-             if(sum1<c);
-       console.log("Yes");
-         else
-       console.log("No");
-         break; 
-             }
-       case 2:{
-             console.log ("Kornelii");
-         break; 
-             }
-       case 3:{
-             console.log ("Vodomir");
-         break; 
-             }
-        default:{
-             console.log ("I don't know");
-         break; 
-             }
+/*var i=0;
+var a=50;
+var sum=0;
+do {
+i=i+2;
+console.log (a);
+sum=a+sum;
 }
+while (i<=a)
+console.log (sum);
+*/
+
+
+
+   
+  var a= {};
+    a.svr1=1,
+    a.svr2=50,
+    a.svr3="world"
+    a.svr4="sirop"
+  
+
+  for (var b in a ){
+    console.log (b);
+    console.log (a[b]);
+  }
+
+
+ 
+
+
+
+
+
+//var a=1;
+//    b=11;
+//    while (a<b) {
+//    sum=a+b; 
+//    a++
+//    console.log ("element",a);
+//    a++
+//  }
+
+
+
+
+
+//var c=4;
+//    sum1=1;
+//switch(1) 
+//{
+//          case 1:{
+//             if(sum1<c);
+//       console.log("Yes");
+//         else
+//       console.log("No");
+//         break; 
+//             }
+//       case 2:{
+//             console.log ("Kornelii");
+//         break; 
+//             }
+//       case 3:{
+//             console.log ("Vodomir");
+//         break; 
+//             }
+//        default:{
+//             console.log ("I don't know");
+//         break; 
+//             }
+//}
+
